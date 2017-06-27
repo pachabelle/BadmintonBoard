68bd5cc76280054416efa0730affcb4580f83ac9
Cheng Lim <cheng.lim@zeacom.com>
2017-06-28 11:10:12 +1200

    more changed

---
+++ BadmintonBoard/BadmintonBoard/Board/GreaterThanZeroToVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace BadmintonBoard.Board
+{
+    public class GreaterThanZeroToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var intValue = (int) value;
+            return intValue > 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
